1b6fac32c47fdd2bab4bd68dec67d6868f77f51a
KikoStar123 <842544563@outlook.com>
2024-09-03 00:59:55 +0800

    merge

---
+++ src/main/java/server/handler/BankLoginRequestHandler.java
@@ -0,0 +1,19 @@
+package server.handler;
+
+import server.service.BankService;
+import org.json.JSONObject;
+
+public class BankLoginRequestHandler implements RequestHandler {
+    @Override
+    public String handle(JSONObject parameters) {
+        BankService bankService = BankService.getInstance();
+        String username = parameters.getString("username");
+        String bankpwd = parameters.getString("bankpwd");
+        JSONObject result = bankService.bankLogin(username, bankpwd);
+
+        JSONObject jsonResponse = new JSONObject();
+        jsonResponse.put("status", result.getString("status"));
+        jsonResponse.put("message", result.getString("message"));
+        return jsonResponse.toString();
+    }
+}
