6278f24eefb2b6578c9585ddcf0b6a33124dbee0
lmy <1173555213@qq.com>
2024-09-02 17:42:27 +0800

    mainui

--- src/main/java/client/ui/StuUI.java
+++ src/main/java/client/ui/StuUI.java
@@ -4,13 +4,20 @@ import client.service.StudentInformation;
 import client.service.User;
 
 import javafx.application.Application;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.geometry.Pos;
 
 import java.net.URL;
 
@@ -21,33 +28,74 @@ public class StuUI extends Application {
         this.user = user;
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
 
-
-
+    public VBox createStudentInfoView() {
         if(user.getUsername().charAt(0)=='0'){
+            StudentInformation.oneStudentInformation[] students = StudentInformation.viewAllStudentInfo();
 
+            TableView<StudentInformation.oneStudentInformation> table = new TableView<>();
+            TableColumn<StudentInformation.oneStudentInformation, String> nameColumn = new TableColumn<>("Name");
+            TableColumn<StudentInformation.oneStudentInformation, String> idColumn = new TableColumn<>("ID");
+            TableColumn<StudentInformation.oneStudentInformation, String> genderColumn = new TableColumn<>("Gender");
+            TableColumn<StudentInformation.oneStudentInformation, String> originColumn = new TableColumn<>("Origin");
+            TableColumn<StudentInformation.oneStudentInformation, String> birthdayColumn = new TableColumn<>("Birthday");
+            TableColumn<StudentInformation.oneStudentInformation, String> academyColumn = new TableColumn<>("Academy");
+
+            nameColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getName()));
+            idColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getId()));
+            genderColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getGender()));
+            originColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getOrigin()));
+            birthdayColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getBirthday()));
+            academyColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getAcademy()));
+
+            table.getColumns().addAll(nameColumn, idColumn, genderColumn, originColumn, birthdayColumn, academyColumn);
+            table.setItems(FXCollections.observableArrayList(students));
+
+            VBox vbox = new VBox();
+            vbox.getChildren().add(table);
+            return vbox;
 
         }else {
+            VBox vbox = new VBox(10); // 设置间距
+            vbox.setAlignment(Pos.CENTER); // 设置对齐方式
 
-            StudentInformation student = new StudentInformation();
-            StudentInformation.oneStudentInformation onestudent = student.viewOneStudentInfo(user.getUsername());
+            // 创建标题文本
+            Text titleText = new Text("我的学籍");
+            titleText.setFont(new Font("Segoe UI Black", 24));
+            vbox.getChildren().add(titleText);
 
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Studentui.fxml"));
-            Parent root = loader.load();
+            // 设置用户信息
+            StudentInformation.oneStudentInformation student = StudentInformation.viewOneStudentInfo(user.getUsername());
 
-            Studentuicontroller controller = loader.getController();
-            controller.setUser(user);
+            // 创建并添加标签
+            Label nameLabel = new Label("姓名: " + student.getName());
+            vbox.getChildren().add(nameLabel);
 
-            primaryStage.setTitle("学生学籍管理");
-            primaryStage.setScene(new Scene(root, 300, 500));
-            primaryStage.show();
+            Label idLabel = new Label("学号: " + student.getId());
+            vbox.getChildren().add(idLabel);
+
+            Label genderLabel = new Label("性别: " + student.getGender());
+            vbox.getChildren().add(genderLabel);
+
+            Label birthLabel = new Label("出生日期: " + student.getBirthday());
+            vbox.getChildren().add(birthLabel);
+
+            Label originLabel = new Label("籍贯: " + student.getOrigin());
+            vbox.getChildren().add(originLabel);
+
+            Label academyLabel = new Label("院系: " + student.getAcademy());
+            vbox.getChildren().add(academyLabel);
+
+            return vbox;
+
         }
     }
     public static void main(String[]args){
         launch(args);
     }
+    @Override
+    public void start(Stage stage) throws Exception {
 
+    }
 }
 
