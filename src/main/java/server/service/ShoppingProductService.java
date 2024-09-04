ea78c0d8ec4c0a84a810c428670e188c58d50e27
KikoStar123 <842544563@outlook.com>
2024-09-04 21:26:44 +0800

    tested shop method

--- src/main/java/server/service/ShoppingProductService.java
+++ src/main/java/server/service/ShoppingProductService.java
@@ -12,7 +12,6 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.UUID;
 
-
 public class ShoppingProductService {
 
     private final Lock getAllProductsLock = new ReentrantLock();
