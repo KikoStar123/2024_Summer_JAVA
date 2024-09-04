a1ad58c5734d3c6c2dd594ffb8c16493b7541e18
杨锦波 <213223414@seu.edu.cn>
2024-09-04 14:56:20 +0800

    change some logic

--- src/main/java/client/MainClient.java
+++ src/main/java/client/MainClient.java
@@ -1,10 +1,12 @@
 package client;
 
+import client.ui.Bankui_Manager;
 import client.ui.LoginUI;
 
 
 public class MainClient {
     public static void main(String[] args) {
+        //Bankui_Manager.main(args);
         LoginUI.main(args);
     }
 }
