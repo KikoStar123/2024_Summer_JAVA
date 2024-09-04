ca09772a2856474753b1493616262ed5ac881d76
hollowlamp <2504384479@qq.com>
2024-09-04 11:08:14 +0800

    update

---
+++ src/test/java/testclient/bank/TestBankPay.java
@@ -0,0 +1,11 @@
+package testclient.bank;
+
+import client.service.Bank;
+
+public class TestBankPay {
+    public static void main(String[] args) {
+        Bank client = new Bank();
+        client.payment("200000001", "password123", "");
+
+
+}
--- src/test/java/testclient/shop/TestShopPay.java
+++ src/test/java/testclient/shop/TestShopPay.java
@@ -6,10 +6,12 @@ import java.io.IOException;
 
 public class TestShopPay {
     public static void main(String[] args) throws IOException {
-        ShoppingOrder order = new ShoppingOrder();
+        ShoppingOrder orderService = new ShoppingOrder();
 
-        order.createOrder("200000001", "123456", 2, 120.5f);
+        String orderID = orderService.createOrder("200000001", "123456", 2, 120.5f);
 
-        order.payOrder("")
+        System.out.println("orderID: " + orderID);
+
+        order.payOrder(orderID);
     }
 }
