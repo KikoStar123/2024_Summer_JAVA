14da8b84211384981863c97b9acd9e74e38ac3b7
杨锦波 <213223414@seu.edu.cn>
2024-09-09 18:14:36 +0800

    滚轮事件处理修改

--- src/main/java/client/ui/MainUI.java
+++ src/main/java/client/ui/MainUI.java
@@ -66,11 +66,11 @@ public class MainUI extends Application {
         bankButton.setOnAction(e -> handleBank(user.getUsername()));
         leftBox.getChildren().add(bankButton);
 
-        Button registerButton = new Button("修改密码");
-        registerButton.setPrefSize(150, 40);
-        registerButton.setStyle("-fx-background-color: #18bcaf; -fx-text-fill: white; -fx-font-size: 16px;");
-        registerButton.setOnAction(e -> handleShop(user.getUsername()));
-        leftBox.getChildren().add(registerButton);
+        Button updateButton = new Button("修改密码");
+        updateButton.setPrefSize(150, 40);
+        updateButton.setStyle("-fx-background-color: #18bcaf; -fx-text-fill: white; -fx-font-size: 16px;");
+        updateButton.setOnAction(e -> handleShop(user.getUsername()));
+        leftBox.getChildren().add(updateButton);
 
         if(user.getRole()==Librarian)
         {
